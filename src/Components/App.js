702c480f1fe16d1553651310d4d0cc46a85873c9
Tahmina <tahminahagverdiyevaa@gmail.com>
2025-03-27 16:07:35 +0100

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo"
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-t from-blue-500 to-purple-500">
+      <Logo />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Logo() {
+
+    return (
+        <header >
+            <h1 className="text-yellow-200 text-5xl font-serif mb-80 mt-0 pt-0">RICHT</h1>
+        </header>
+    )
+};
+
+export default Logo;
